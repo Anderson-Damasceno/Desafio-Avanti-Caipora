0fc8be9d44359fc06df49292c7b6e1bf703be7b1
demetrio <demetriolins@hotmail.com>
2023-09-09 09:55:36 -0300

    Adicionando comentarios

--- src/models/jogadorModel.ts
+++ src/models/jogadorModel.ts
@@ -1,7 +1,9 @@
 import { prismaClient } from "../database/prismaClient";
-
+/*
+Temos uma classe com métodos cuja funcionalidade é lidar com Banco de Dados
+*/
 class JogadorModel {
-
+//Método para criar no banco de dados uma entidade do tipo jogador
     public async create(id: number, nome: string, nascimento: Date, timeId: number) {
 
         const jogador = await prismaClient.jogador.create({
@@ -15,7 +17,7 @@ class JogadorModel {
     
         return jogador;    
     }
-    
+    //Método para encontrar uma Entidade apartir do Id
     public async findById(id: number) {
     
         const jogador = await prismaClient.jogador.findUnique({
@@ -25,7 +27,7 @@ class JogadorModel {
         return jogador;
         
     }
-    
+    //Método para encontrar todas as Entidades que estão no banco de dados
     public async findAll() {
 
         const jogadores = await prismaClient.jogador.findMany();
@@ -33,7 +35,7 @@ class JogadorModel {
         return jogadores;
         
     }
-
+//Método para alterar propriedades em alguma Entidade apartir do Id no banco de dados
     public async update(id: number, nome: string, nascimento: Date, timeId: number) {
 
         const jogador = await prismaClient.jogador.update({
@@ -48,7 +50,7 @@ class JogadorModel {
     
         return jogador;    
     }
-
+//Método para deletar entidade no banco de dados apartir do Id
     public async delete(id: number) {
 
         const jogador = await prismaClient.jogador.delete({
