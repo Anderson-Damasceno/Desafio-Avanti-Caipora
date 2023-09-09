2efc4694b140fb46953c27a823176e8791233439
Pedryn <franciscopedroficial@gmail.com>
2023-09-09 10:34:37 -0300

    campeonato tudo certo e comentado

--- src/models/campeonatoModel.ts
+++ src/models/campeonatoModel.ts
@@ -1,7 +1,9 @@
 import { prismaClient } from "../database/prismaClient";
 
+/*Temos uma classe com métodos cuja funcionalidade é lidar com Banco de Dados*/
 class CampeonatoModel {
 
+    //Método para criar no banco de dados uma entidade do tipo campeonato
     public async create(id: number, nome: string, inicio: Date, fim:Date) {
 
         const campeonato = await prismaClient.campeonato.create({
@@ -15,7 +17,8 @@ class CampeonatoModel {
     
         return campeonato;    
     }
-    
+
+    //Método para encontrar uma Entidade apartir do Id
     public async findById(id: number) {
     
         const campeonato = await prismaClient.campeonato.findUnique({
@@ -25,7 +28,8 @@ class CampeonatoModel {
         return campeonato;
         
     }
-    
+
+    //Método para encontrar todas as Entidades que estão no banco de dados
     public async findAll() {
 
         const campeonato = await prismaClient.campeonato.findMany();
@@ -34,6 +38,7 @@ class CampeonatoModel {
         
     }
 
+    //Método para alterar propriedades em alguma Entidade apartir do Id no banco de dados
     public async update(id: number, nome: string, inicio:Date, fim:Date) {
 
         const campeonato = await prismaClient.campeonato.update({
@@ -48,7 +53,8 @@ class CampeonatoModel {
     
         return campeonato;    
     }
-
+    
+    //Método para deletar entidade no banco de dados apartir do Id
     public async delete(id: number) {
 
         const campeonato = await prismaClient.campeonato.delete({
@@ -59,3 +65,5 @@ class CampeonatoModel {
 }
 
 export { CampeonatoModel };
+
+// Obs.: Os dados do tipo Date devem ser preenchidos no formato ISO 8601 - YYYY-MM-DDTHH:MM:SS.000Z
--- src/routes/campeonatoRouter.ts
+++ src/routes/campeonatoRouter.ts
@@ -4,6 +4,12 @@ import { CampeonatoController } from "../controllers/campeonatoController";
 const campeonatoRouter = Router();
 const campeonatoController = new CampeonatoController();
 
+/*
+Definindo funções cujo objetivo é estabelecer rotas de acesso aos métodos
+da classe campeonatoContoller. Aqui sao definidas as rotas que o cliente fará uso
+para acessar ou modificar os dados desejados.
+*/
+
 campeonatoRouter.post("/campeonato", campeonatoController.create);
 campeonatoRouter.get("/campeonato/:id", campeonatoController.find);
 campeonatoRouter.get("/campeonato", campeonatoController.findAll);
